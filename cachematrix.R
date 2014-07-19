12c99d04e9932ef32a6262b5cf6f22023536b54f
teenie-m <teenie-m@users.noreply.github.com>
2014-07-19 18:43:27 -0400

    completed assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## based heavily on makeVector and cachemean from Assignment 2 description
+## code adjusted to take the inverse of a matrix rather than the mean of a vector
+## "m" and "mean" variables changed to "inverse.cache" and "inverse"
 
-## Write a short comment describing this function
+## returns functions that set/retrieve matrix (x) and its inverse (inverse.cache)
+## x and inverse.cache are "private" variables
+## i.e. they exist only within the environment defined by makeCacheMatrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse.cache <- NULL
+  set <- function(y) {
+    x <<- y # <<- sets x and inverse.cache in parent (makeCacheMatrix) environment
+    inverse.cache <<- NULL
+  }
+  get <- function() x
+  set.inverse <- function(inverse) inverse.cache <<- inverse
+  get.inverse <- function() inverse.cache
+  list(set = set, get = get,
+       set.inverse = set.inverse,
+       get.inverse = get.inverse)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve takes list defined by makeCacheMatrix as input
+## calls functions therein to set and/or retrieve inverse.cache variable
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$get.inverse()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  matrix.in <- x$get()
+  inverse <- solve(matrix.in, ...)
+  x$set.inverse(inverse)
+  inverse #returns inverse whether previously cached or not
 }
